450d45fd688ad249bc2a7a757c72fbd871e52b5f
Rabbit <515236489@qq.com>
2017-07-11 23:09:01 +0800

    update

--- Test/Detail1.js
+++ Test/Detail1.js
@@ -18,7 +18,7 @@ export default class Detail1 extends Component {
         // 这里面的属性和App.js的navigationOptions是一样的。
         headerTitle:navigation.state.params?navigation.state.params.headerTitle:'Detail1',
         headerRight:(
-            <Text style={{color:'red',marginRight:20}} onPress={()=>navigation.state.params.navigatePress()}>我的</Text>
+            <Text style={{color:'red',marginRight:20}} onPress={()=>navigation.state.params?navigation.state.params.navigatePress():null}>我的</Text>
         ),
     });
 
